721c438e52d045a507b2eaeedb56e1b8f061c3f1
lichengjun <1062432262@qq.com>
2017-03-28 10:04:53 +0800

    update

--- src/main/java/java1702/javase/collection/_2DArray.java
+++ src/main/java/java1702/javase/collection/_2DArray.java
@@ -6,13 +6,43 @@ package java1702.javase.collection;
  */
 public class _2DArray {
     public static void main(String[] args) {
-        int[][] ints = new int[8][9];
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 9; j++) {
+
+//        int[][] ints = new int[8][9];
+//        for (int i = 0; i < 8; i++) {
+//            for (int j = 0; j < 9; j++) {
+//                ints[i][j] = i + j;
+//                System.out.print(ints[i][j] + "\t");
+//            }
+//            System.out.println();
+//        }
+
+        int[][] ints = new int[5][7];
+        for (int i = 0; i < ints.length; i++) {
+            for (int j = 0; j < ints[i].length; j++) {
                 ints[i][j] = i + j;
-                System.out.print(ints[i][j] + "\t");
+                System.out.println(ints[i] + "\t");
+            }
+        }
+        String[][] strings = new String[4][];
+        strings[0] = new String[5];
+        strings[1] = new String[1];
+        strings[2] = new String[3];
+        strings[3] = new String[4];
+
+        double[][] doubles =
+                {
+                        {1d,2d,3d,4d},
+                        {1d},
+                        {1d,2d},
+                        {1d,2d,3d},
+
+                };
+        for (int i = 0; i < doubles.length; i++) {
+            for (int j = 0; j < doubles[i].length; j++) {
+                System.out.println(doubles[i] + "\t");
             }
             System.out.println();
         }
+
     }
 }
