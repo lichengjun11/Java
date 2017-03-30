defd2ce744a3f9cd80d8936d47c750fd7e49cf95
lichengjun <1062432262@qq.com>
2017-03-30 16:30:09 +0800

    update

---
+++ src/main/java/java1702/javase/collection/Array1.java
@@ -0,0 +1,12 @@
+package java1702.javase.collection;
+
+/**
+ * Created by Li chengjun
+ * on 2017/3/30 16:18.
+ */
+public class Array1 {
+    String[] aArray = new String[5];
+    String[] bArray = {"a","b","c","d","e"};
+    String[] cArray = new String[]{"a","b","c","d","e"};
+
+}
