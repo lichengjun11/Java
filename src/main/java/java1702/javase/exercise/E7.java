c7b9c44a22374031facefc0822b03cae8aa18588
lichengjun <1062432262@qq.com>
2017-03-30 19:13:41 +0800

    update

--- src/main/java/java1702/javase/exercise/E7.java
+++ src/main/java/java1702/javase/exercise/E7.java
@@ -10,29 +10,11 @@ import java.util.Scanner;
 // 输入一行字符，分别统计出其中英文字母、空格、数字和其它字符的个数。
 public class E7 {
     public static void main(String[] args) {
+        //首先 接收用户从控制台输入的一行字符
         Scanner scanner = new Scanner(System.in);
         System.out.println("input string:");
-        String string = scanner.nextLine();
+        String string = scanner.nextLine();//定义一个字符串类的变量string，独取一行。。可以获取用户从控制台输入的字符
+        System.out.println(string);
 
-        char[] chars = string.toCharArray();
-        int english = 0;
-        int space = 0;
-        int digit = 0;
-        int special = 0;
-        for (char aChar : chars) {
-            if (((aChar >= 'A' && aChar <= 'Z') || (aChar >= 'a' && aChar <= 'z'))) {
-                english++;
-            } else if (aChar == ' ') {
-                space++;
-            } else if (aChar >= '0' && aChar <= '9') {
-                digit++;
-            } else {
-                special++;
-            }
-        }
-        System.out.println(english);
-        System.out.println(space);
-        System.out.println(digit);
-        System.out.println(special);
     }
 }
