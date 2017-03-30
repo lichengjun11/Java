c6f321ae9fbb60e380e319140f9bce078ba64358
lichengjun <1062432262@qq.com>
2017-03-30 19:35:28 +0800

    update

--- src/main/java/java1702/javase/exercise/E7.java
+++ src/main/java/java1702/javase/exercise/E7.java
@@ -16,5 +16,27 @@ public class E7 {
         String string = scanner.nextLine();//定义一个字符串类的变量string，独取一行。。可以获取用户从控制台输入的字符
         System.out.println(string);
 
+        char[] chars = string.toCharArray();
+        int english = 0; // 英语
+        int space = 0; // 空格
+        int digit = 0; // 数字
+        int special = 0;
+        for (char aChar : chars) {
+            if ((aChar>='A'&&aChar<='Z')||(aChar>='a'&&aChar<='z')) {
+                english++;
+            } else if (aChar == ' ') {
+                space++;
+            } else if (aChar >= '0' && aChar <= '9') {
+                digit++;
+            } else {
+                special++;
+            }
+            }
+        System.out.println(english);
+        System.out.println(space);
+        System.out.println(digit);
+        System.out.println(special);
+        }
+
+
     }
-}
