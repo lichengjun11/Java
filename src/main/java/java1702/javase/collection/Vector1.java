c0cbe329a88c1213f1e1b437141e684c29e7d398
lichengjun <1062432262@qq.com>
2017-03-30 17:13:54 +0800

    update

---
+++ src/main/java/java1702/javase/collection/Vector1.java
@@ -0,0 +1,34 @@
+package java1702.javase.collection;
+
+import java.util.Vector;
+
+/**
+ * Created by Li chengjun
+ * on 2017/3/30 16:57.
+ */
+public class Vector1 {
+    public static void main(String[] args) {
+        Vector<String> vector = new Vector<>();
+                                                //JDK5
+        vector.add("hi");  // 添加
+        vector.add("hello");
+        vector.add("test");
+        vector.add("hi");
+
+        System.out.println(vector.size());
+        System.out.println(vector.get(0));
+        System.out.println(vector.remove(2));
+        System.out.println(vector.capacity());
+        System.out.println(vector.size());
+        for (String s : vector) {
+            System.out.println(s);
+        }
+        Vector<Integer> integers = new Vector<>();
+        integers.add(32);
+        integers.add(345);
+        integers.add(333);
+        System.out.println(integers.size());
+        System.out.println(integers.capacity());
+        integers.setElementAt(66,0);  // 修改;
+    }
+}
