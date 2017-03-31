bf60de3913f1939b4c889c739d7f6790b163994a
lichengjun <1062432262@qq.com>
2017-03-31 17:16:38 +0800

    update

--- src/main/java/java1702/javase/collection/ListTest1.java
+++ src/main/java/java1702/javase/collection/ListTest1.java
@@ -20,6 +20,9 @@ public class ListTest1 {
         System.out.println(list.lastIndexOf("hello"));
 
         System.out.println(list.isEmpty());
+        System.out.println(list.remove(1));
+        System.out.println(list.set(1,"lilili"));
+        System.out.println(list.toString());
 
     }
 
