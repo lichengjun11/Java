8a0af029486a1e6df3228dcea2a49d93aeb8df5b
lichengjun <1062432262@qq.com>
2017-03-31 17:01:49 +0800

    update

---
+++ src/main/java/java1702/javase/collection/ListTest1.java
@@ -0,0 +1,21 @@
+package java1702.javase.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Li chengjun
+ * on 2017/3/31 16:53.
+ */
+public class ListTest1 {
+    public static void main(String[] args) {
+    ArrayList<String> list = new ArrayList<>();
+    list.add("hi");
+    list.add("hello");
+    list.add("hello");
+        System.out.println(list.size());
+        System.out.println(list.get(0));
+
+    }
+
+}
