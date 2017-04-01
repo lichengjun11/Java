818c3770543738fbe0c2082466c6359126f44963
lichengjun <1062432262@qq.com>
2017-04-01 10:03:36 +0800

    update

---
+++ src/main/java/java1702/javase/oop/NewHuman.java
@@ -0,0 +1,31 @@
+package java1702.javase.oop;
+
+/**
+ * Created by Li chengjun
+ * on 2017/4/1 9:00.
+ */
+public class NewHuman {
+    String name;
+    char gender;
+    int age;
+    double weight;
+    double height;
+
+    public void eat() {
+
+    }
+    public void sleep() {
+
+    }
+    public String study(int studyTime) {
+        System.out.println(name+"xuexile");
+        return"一份英语作业";
+    }
+    public void work() {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
