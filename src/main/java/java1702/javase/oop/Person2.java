fa446c09c2f4181cf04324e20205071bafc28be0
lichengjun <1062432262@qq.com>
2017-04-05 21:03:25 +0800

    update

---
+++ src/main/java/java1702/javase/oop/Person2.java
@@ -0,0 +1,35 @@
+package java1702.javase.oop;
+
+/**
+ * Created by Li chengjun
+ * on 2017/4/5 20:53.
+ */
+public class Person2 {
+    public String name = "xiaomiao";
+
+    public Person2(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public int age = 18;
+}
+
+class Student2 extends Person2 {
+    public Student2(String name, int age) {
+        super(name, age);
+    }
+
+    void study() {
+        System.out.println("I can study!");
+    }
+}
+
+class JiCheng {
+    public static void main(String[] args) {
+        Student2 student2 = new Student2("王飞",30);
+        Person2 person2 = new Person2("李非",33);
+        System.out.println("name="+student2.name+"..."+"age="+student2.age);
+
+    }
+}
