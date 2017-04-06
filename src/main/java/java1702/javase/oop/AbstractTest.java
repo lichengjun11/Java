640a5e29584e46a9cacf5fce3a34c8033a3aa23b
lichengjun <1062432262@qq.com>
2017-04-06 19:06:26 +0800

    update

--- src/main/java/java1702/javase/oop/AbstractTest.java
+++ src/main/java/java1702/javase/oop/AbstractTest.java
@@ -5,28 +5,37 @@ package java1702.javase.oop;
  * on 2017/4/6 9:13.
  */
 public abstract class AbstractTest {
+    SubAbstractTest subAbstractTest = new SubAbstractTest(3);
+
     public AbstractTest(int i) {
         this.i = i;
     }
 
     private int i;
-    public abstract void method(String s);
 
-    public static void main(String[] args) {
-        SubAbstractTest subAbstractTest = new SubAbstractTest(5);
+
+
+    public abstract void method();
+    public abstract void method(String s);
+    public void m() {
+
     }
 }
 
 class SubAbstractTest extends AbstractTest {
+
 
     public SubAbstractTest(int i) {
         super(i);
     }
 
     @Override
-    public void method(String s) {
+    public void method() {
 
     }
 
+    @Override
+    public void method(String s) {
 
+    }
 }
