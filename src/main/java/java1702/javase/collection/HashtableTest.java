65c4c2da3ef83b7f924a28af9a91898a181e9284
lichengjun <1062432262@qq.com>
2017-04-10 09:17:28 +0800

    update

--- src/main/java/java1702/javase/collection/HashtableTest.java
+++ src/main/java/java1702/javase/collection/HashtableTest.java
@@ -15,20 +15,23 @@ public class HashtableTest {
        hashtable.put(123,"你好");
        hashtable.put(3,"good");
 
+       hashtable.put(null,"ww");
+       hashtable.put(12,null);  //  当键和值为null时，虽然编译不报错，但运行时报错
+
         System.out.println(hashtable.size());
         System.out.println(hashtable.get(123));
 
         System.out.println(hashtable.remove(11));
 
         for (Integer integer : hashtable.keySet()) {
-            System.out.println(integer + "-->" + hashtable.get(integer));   // ?
+            System.out.println(integer + "-->" + hashtable.get(integer));   // 获取键的集合
 
             for (String s : hashtable.values()) {
-                System.out.println(s);
+                System.out.println(s);              //  获取值的集合
             }
 
             for (Map.Entry<Integer, String> integerStringEntry : hashtable.entrySet()) {
-                System.out.println(integerStringEntry.getKey() +"--->"+ integerStringEntry.getValue());
+                System.out.println(integerStringEntry.getKey() +"--->"+ integerStringEntry.getValue());  // 获取键和值的集合
             }
         }
 
--- src/main/java/java1702/javase/collection/StringTest2.java
+++ src/main/java/java1702/javase/collection/StringTest2.java
@@ -18,25 +18,20 @@ public class StringTest2 {
             System.out.println(s.toLowerCase());
             System.out.println(s.toUpperCase());
             System.out.println(s.concat(s1));// 连接
-
             String s5 = "你好";
             for (byte b : s5.getBytes()) {//字符串返回字节数组，可以进行迭代，用foreath循环
                 System.out.println(b); //一个汉字返回三个字节。一个字母返回一个字节
-
                 String s6 = "hi Hello";
                 System.out.println(s6.indexOf("e")); // indexOf 返回子字符串e在s6字符串中第一次出现的索引
-
                 s6 = "";
                 System.out.println(s6.isEmpty());  // 判断是不是空字符串
                 String s7 = "hi liChengJun";
                 System.out.println(s7.matches("\\w+"));
                 System.out.println(s7.replaceAll("\\w+","a"));
-
                 String[] strings = s7.split("\\s"); // space 空格 \\s+
                 System.out.println(strings.length);
                 for (String string : strings) {
                     System.out.println(string); //  拆分字符串
-
                     System.out.println(s7.substring(s.indexOf("l"),s.indexOf("l")+3));// 提取一个子字符串
                     System.out.println(String.valueOf(1.223));// 把其他类型的转换为字符串
                 }
