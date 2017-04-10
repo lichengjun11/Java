f3dcada8311491249ace955dd157ccc5e3c24014
lichengjun <1062432262@qq.com>
2017-04-10 21:56:01 +0800

    update

--- src/main/java/java1702/javase/collection/ArrayTest1.java
+++ src/main/java/java1702/javase/collection/ArrayTest1.java
@@ -13,10 +13,17 @@ public class ArrayTest1 {
         list.add("a");
         list.add("b");
         list.add("c");
-        System.out.println("顺序添加：" + list);
-
-        list.add(1,"E");// 在第一个元素后边添加E
-        list.add(4,"F"); // 在第三个后边添加F
+        List<String> list1 = new ArrayList<>();
+        list1.add("大");
+        list1.add("天");
+        list1.add("火");
+        list1.addAll(1,list); //  将list全部添加到list1中
+        System.out.println("顺序添加：" + list1);
+        System.out.println(list.remove(list));
         System.out.println(list);
+
+//        list.add(1,"E");// 在第一个元素后边添加E
+//        list.add(4,"F"); // 在第三个后边添加F
+//        System.out.println(list);
     }
 }
