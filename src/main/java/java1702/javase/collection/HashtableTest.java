c862f3eec2bd2d4548e882df7a2f60aa81a1e90a
lichengjun <1062432262@qq.com>
2017-04-10 09:05:19 +0800

    update

--- src/main/java/java1702/javase/collection/HashtableTest.java
+++ src/main/java/java1702/javase/collection/HashtableTest.java
@@ -1,6 +1,7 @@
 package java1702.javase.collection;
 
 import java.util.Hashtable;
+import java.util.Map;
 
 /**
  * Created by Li chengjun
@@ -8,7 +9,28 @@ import java.util.Hashtable;
  */
 public class HashtableTest {
     public static void main(String[] args) {
-        // k  key 键     v  value  值
+       Hashtable<Integer,String> hashtable = new Hashtable<>();
+       hashtable.put(1,"hi");
+       hashtable.put(11,"hello");
+       hashtable.put(123,"你好");
+       hashtable.put(3,"good");
+
+        System.out.println(hashtable.size());
+        System.out.println(hashtable.get(123));
+
+        System.out.println(hashtable.remove(11));
+
+        for (Integer integer : hashtable.keySet()) {
+            System.out.println(integer + "-->" + hashtable.get(integer));   // ?
+
+            for (String s : hashtable.values()) {
+                System.out.println(s);
+            }
+
+            for (Map.Entry<Integer, String> integerStringEntry : hashtable.entrySet()) {
+                System.out.println(integerStringEntry.getKey() +"--->"+ integerStringEntry.getValue());
+            }
+        }
 
     }
 }
