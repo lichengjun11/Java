1efe5c9ce5b249ad32ed5a9bad95cd691cb6c017
lichengjun <1062432262@qq.com>
2017-04-10 20:22:42 +0800

    update

---
+++ src/main/java/java1702/javase/collection/ArrayTest1.java
@@ -0,0 +1,22 @@
+package java1702.javase.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Li chengjun
+ * on 2017/4/10 20:13.
+ */
+public class ArrayTest1 {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("a");
+        list.add("b");
+        list.add("c");
+        System.out.println("顺序添加：" + list);
+
+        list.add(1,"E");// 在第一个元素后边添加E
+        list.add(4,"F"); // 在第三个后边添加F
+        System.out.println(list);
+    }
+}
---
+++ src/main/java/java1702/javase/collection/StudentTest1.java
@@ -0,0 +1,18 @@
+package java1702.javase.collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Li chengjun
+ * on 2017/4/10 19:34.
+ */
+//编写一个学生类，将学生的姓名，年龄，性别分别保存到 HashSet ArrayList
+  //  Hashtable 中，并利用迭代器 Iterator 将姓名，年龄，性别打印出来
+public class StudentTest1 {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+
+
+    }
+}
