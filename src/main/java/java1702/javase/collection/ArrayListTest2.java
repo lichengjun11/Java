f12e3519cb570ad675fd3aa34452f16a07bee469
lichengjun <1062432262@qq.com>
2017-04-11 20:54:55 +0800

    update

---
+++ src/main/java/java1702/javase/collection/ArrayListTest2.java
@@ -0,0 +1,47 @@
+package java1702.javase.collection;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by Li chengjun
+ * on 2017/4/11 20:31.
+ */
+public class ArrayListTest2 {
+    public static void main(String[] args) {
+        ArrayList<String> arrayList =  new ArrayList<>();
+        arrayList.add("java");
+        arrayList.add("web");
+        arrayList.add("abc");
+
+        ArrayList<String> arrayList1 =  new ArrayList<>();
+        arrayList1.add("java");
+        arrayList1.add("abc");
+        arrayList1.add("web");
+//        System.out.println(arrayList.addAll(arrayList));
+        System.out.println(arrayList.contains("a"));
+        System.out.println(arrayList.get(0));
+        System.out.println(arrayList.indexOf("web"));
+        System.out.println(arrayList.lastIndexOf("web"));
+        System.out.println(Arrays.toString(arrayList.toArray()));
+        System.out.println(arrayList.size());
+        arrayList.trimToSize();
+
+        System.out.println(arrayList.equals(arrayList1));
+
+        List<String> strings3 = arrayList.subList(1,3);
+        System.out.println(strings3);
+        System.out.println(arrayList.subList(0,1));
+        System.out.println(arrayList.containsAll(strings3));
+
+//        arrayList.removeAll(strings3);
+//        System.out.println(arrayList);  //  返回arrayList和strings3 不重复的那部分
+
+          arrayList.retainAll(strings3);
+        System.out.println(arrayList);  // 返回arrayList 和strings3 重复的那部分
+
+
+
+    }
+}
