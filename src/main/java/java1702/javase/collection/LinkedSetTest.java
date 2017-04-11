4aa0a55c852e891d7edb7a1476a5253205a1fd4e
lichengjun <1062432262@qq.com>
2017-04-11 16:16:06 +0800

    update

--- src/main/java/java1702/javase/collection/LinkedSetTest.java
+++ src/main/java/java1702/javase/collection/LinkedSetTest.java
@@ -1,11 +1,26 @@
 package java1702.javase.collection;
 
+import java.util.LinkedHashSet;
+
 /**
  * Created by Li chengjun
  * on 2017/4/11 16:10.
  */
+// linkedHashSet  是有序的，不可重复的
 public class LinkedSetTest {
     public static void main(String[] args) {
-        link
+        LinkedHashSet<String> strings = new LinkedHashSet<>();
+        strings.add("a");
+        strings.add("b");
+        strings.add("c");
+        strings.add("d");
+        strings.add("e");
+        strings.add("f");
+        strings.add("f");
+        strings.add("g");
+        System.out.println(strings.size());
+        for (String string : strings) {
+            System.out.println(string);
+        }
     }
 }
