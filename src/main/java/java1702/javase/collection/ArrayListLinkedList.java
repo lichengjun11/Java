b06dee3da1fa037b68e0439dd8c1eedd1650990d
lichengjun <1062432262@qq.com>
2017-04-11 23:38:31 +0800

    update

--- src/main/java/java1702/javase/collection/ArrayListLinkedList.java
+++ src/main/java/java1702/javase/collection/ArrayListLinkedList.java
@@ -11,20 +11,27 @@ public class ArrayListLinkedList {
     public static void main(String[] args) {
         ArrayList<String> arrayList = new ArrayList<>();
         LinkedList<String> linkedList = new LinkedList<>();
+        //运行时  totalmemory 总共的内存空间   freememory  空闲的内存空间
+       long m1 = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+        System.out.println(m1);
         for (int i = 0; i < 10000; i++) {
             arrayList.add( i+"");
             linkedList.add(i + "");
         }
-        long start = System.currentTimeMillis();
-//            System.out.println(arrayList);
-        System.out.println(arrayList.get(1));
-        long time1  = System.currentTimeMillis();
+        long m2 = Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory();
+        System.out.println(m2);
+        System.out.println((m2-m1)/1024/1024);
 
-        System.out.println(linkedList.get(1));
-        long time2  = System.currentTimeMillis();
-
-        System.out.println(time1 - start);
-        System.out.println(time2 - time1);
+//        long start = System.currentTimeMillis();
+            System.out.println(arrayList);
+//        System.out.println(arrayList.get(1));
+//        long time1  = System.currentTimeMillis();
+//
+//        System.out.println(linkedList.get(1));
+//        long time2  = System.currentTimeMillis();
+//
+//        System.out.println(time1 - start);
+//        System.out.println(time2 - time1);
 
     }
 }
