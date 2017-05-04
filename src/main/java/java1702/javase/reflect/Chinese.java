0f44464621301f7b1404120404d6d17669c8df95
lichengjun <1062432262@qq.com>
2017-05-04 19:47:37 +0800

    update

--- src/main/java/java1702/javase/reflect/Chinese.java
+++ src/main/java/java1702/javase/reflect/Chinese.java
@@ -1,8 +1,24 @@
 package java1702.javase.reflect;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Li chengjun
  * on 2017/5/3 17:31.
  */
 public class Chinese {
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            list.add(1);
+        }
+        list.add(0);
+        System.out.println(list.size());
+
+        Field array = list.getClass().getDeclaredField("elementData");
+        array.setAccessible(true);
+        System.out.println(((Object[])array.get(list)).length);
+    }
 }
--- src/main/java/java1702/javase/reflect/Human.java
+++ src/main/java/java1702/javase/reflect/Human.java
@@ -1,12 +1,18 @@
 package java1702.javase.reflect;
 
+import org.omg.Dynamic.Parameter;
+
+import java.lang.reflect.Constructor;
+
 /**
  * Created by Li chengjun
  * on 2017/5/3 17:32.
  */
 
-class Animals{
+class Animals {
+
     public int age;
+
     private double weight;
 
     public Animals() {
@@ -16,17 +22,19 @@ class Animals{
         this.age = age;
         this.weight = weight;
     }
-    public int sleep(int hours){
+
+    public int sleep(int hours) {
         return hours;
     }
-    public void eat(String food){
-        System.out.println("eating"+food);
+
+    public void eat(String food) {
+        System.out.println("eating " + food);
     }
 
-    private void killHuman(){
+    private void killHuman() {
         System.out.println("killed a poor guy...");
-
     }
+
     public int getAge() {
         return age;
     }
@@ -48,6 +56,9 @@ public class Human extends Animals {
     public String name;
     private boolean married;
 
+    Human() {
+    }
+
     public Human(int age, double weight, String name, boolean married) {
         super(age, weight);
         this.name = name;
@@ -56,9 +67,58 @@ public class Human extends Animals {
 
     @Override
     public void eat(String food) {
-        System.out.println(name+"is now eating"+food);
+        System.out.println(name + " is now eating " + food);
     }
-    public void study(String course){
-        System.out.println(name + "is now killing " + course);
+
+    public void study(String course) {
+        System.out.println(name + " is now studying " + course);
+    }
+
+    private void killAnimals(String animal) {
+        System.out.println(name + " is now killing " + animal);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isMarried() {
+        return married;
+    }
+
+    public void setMarried(boolean married) {
+        this.married = married;
+    }
+}
+class HumanTest{
+
+
+    public static void main(String[] args) throws NoSuchMethodException {
+        Class clazz = Human.class;
+        
+        Constructor[] constructors = clazz.getConstructors();
+        System.out.println("-- getConstructors() ---");
+        for (Constructor constructor : constructors) {
+            System.out.println(constructor.getName());
+            for (java.lang.reflect.Parameter parameter : constructor.getParameters()) {
+                System.out.println("\t" + parameter.getName());
+            }
+        }
+        Constructor[] declaredConstructors = clazz.getDeclaredConstructors();
+        System.out.println("---getDeclaredConstructors---");
+        for (Constructor declaredConstructor : declaredConstructors) {
+            System.out.println(declaredConstructor.getName());
+            for (java.lang.reflect.Parameter parameter : declaredConstructor.getParameters()) {
+                System.out.println("\t" + parameter.getName());
+            }
+        }
+
+        Constructor constructor = clazz.getDeclaredConstructor(int.class,double.class,String.class,boolean.class);
+        System.out.println(constructor.getName());
+
     }
 }
