2d60ad8393cecf695dc04e91305f8f7bd27e6a01
lichengjun <1062432262@qq.com>
2017-05-10 11:17:56 +0800

    update

--- src/main/java/java1702/javase/test/GuZiWang.java
+++ src/main/java/java1702/javase/test/GuZiWang.java
@@ -2,8 +2,11 @@ package java1702.javase.test;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 import java.io.IOException;
 
 /**
@@ -11,9 +14,33 @@ import java.io.IOException;
  * on 2017/5/10 9:35.
  */
 public class GuZiWang {
-    public static void main(String[] args) throws IOException {
-    String url = "https://www.guazi.com/bj/audi/";
+    private static final String url = "https://www.guazi.com/bj/audi/";
+    private static int counter;
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 8; i++) {
+            System.out.println("page:" + (i + 1) );
+            try {
+                page(i + 1);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    private static void page(int page) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("data/guziwang",true))){
+
         Document document = Jsoup.connect(url).get();
-        Elements elements = document.select("p[class=infoBox]");
+        Elements elements = document.select("div[class=list-infoBox]");
+        for (Element element : elements) {
+            String s = element.select("p[class=infoBox]").first().text();
+            String jiage = element.select("i[class=fc-org priType]").first().text();
+            String tupian = element.select("img[src]").attr("src");
+//            System.out.println("车型：   " + s + "   价格：    " + jiage);
+//            System.out.println(tupian);
+            writer.write("车型：   " + s + "   价格：    " + jiage + "\n");
+            System.out.println("\t" + "counter:"+ ++counter);
+        }
+        }
     }
 }
